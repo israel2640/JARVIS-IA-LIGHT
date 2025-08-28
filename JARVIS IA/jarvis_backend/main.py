bd4f9dc1d606157cbf75cf35caaa5c9010e723ba
Israel Paz <95177192+israel2640@users.noreply.github.com>
2025-08-27 23:24:06 -0300

    Update main.py

--- JARVIS IA/jarvis_backend/main.py
+++ JARVIS IA/jarvis_backend/main.py
@@ -86,12 +86,18 @@ class ChatInput(BaseModel):
 # ==========================================================
 app = FastAPI(title="Jarvis IA Backend")
 
+# Lista de origens permitidas
+origins = [
+    "https://jarvis-ia-frontend.onrender.com",
+    "http://127.0.0.1:5500",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://jarvis-ia-frontend.onrender.com", "http://localhost:5500"],
+    allow_origins=origins, 
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"], 
+    allow_headers=["*"], 
 )
 
 # ==========================================================
